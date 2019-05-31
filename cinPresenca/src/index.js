95118f275e9761c578fc1605f6b21635c37fcf37
Emmanuel312 <emmanuelfelipe@viitra.com>
2019-05-30 23:51:47 -0300

    configuracao ble + firebase e estrutura de rotas

---
+++ cinPresenca/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+import Routes from './routes'
+
+
+const App = () =>
+(
+    <>
+        <StatusBar  backgroundColor="#50B4F2" barStyle="light-content"/>
+        <Routes />
+    </>
+)
+
+export default App
---
+++ cinPresenca/src/routes.js
@@ -0,0 +1,20 @@
+import { createAppContainer,createSwitchNavigator, createBottomTabNavigator } from 'react-navigation'
+import Login from './screens/Login'
+import Register from './screens/Register'
+import Main from './screens/Main'
+
+const MainBottom = createBottomTabNavigator(
+{
+    Main
+})
+
+const Routes = createSwitchNavigator(
+{
+    Login,
+    Register,
+    MainBottom
+
+})
+
+
+export default createAppContainer(Routes)
