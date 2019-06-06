3c877a031813441317260d9003272df782489113
Emmanuel312 <emmanuelfelipe@viitra.com>
2019-06-06 20:05:02 -0300

    adicionando animaçao

--- cinPresenca/src/screens/Main/index.js
+++ cinPresenca/src/screens/Main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { View,Text,Animated,NativeEventEmitter,NativeModules,} from 'react-native'
+import { NativeEventEmitter,NativeModules} from 'react-native'
 import { Container,AlertText } from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import BleManager from 'react-native-ble-manager'
 import firebase from 'react-native-firebase';
+import * as Animatable from 'react-native-animatable'
+import enableLocation from '../../Components/enableLocation'
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
@@ -18,28 +20,40 @@ export default class Login extends Component
     }
     async componentDidMount()
     {
+        
         const user = await firebase.auth().currentUser
         this.setState({user})
         this.handlerDiscover = bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral );
+       
         BleManager.enableBluetooth()
-        .then(() => {
+        .then(() => 
+        {
             console.log('The bluetooth is already enabled or the user confirm')
             BleManager.start({showAlert: true})
-            .then(() => {
+            .then(() =>
+            {
                 console.log('Module Started')
+            })
+            .catch((error) =>
+            {
+                // Failure code
+                console.log('Module doesnt started')
+            })
         })
-        .catch((error) => {
+        .catch((error) =>
+        {
             // Failure code
             console.log('The user refuse to enable bluetooth')
         })
-        })
+        enableLocation()
+       
         this.interval = setInterval(() => this.scan(),10000)
             
     }
 
     componentWillUnmount()
     {
-        clearInterval(this.interval);
+        //clearInterval(this.interval);
         this.handlerDiscover.remove();
     }
 
@@ -49,7 +63,6 @@ export default class Login extends Component
             console.log(results)
         }).catch(err => console.log(err))
         
-
     }
     handleDiscoverPeripheral = async peripheral =>
     {
@@ -58,16 +71,25 @@ export default class Login extends Component
         {
             console.log('Got ble peripheral', peripheral);
             peripherals.set(peripheral.id, peripheral);
-            this.setState({ peripherals })
-            
+            this.setState({ peripherals })   
         }
-        await firebase.database().ref(`usersTeste/${this.state.user.uid}/${peripheral.id}`).set({timestamp: Date.now(),uuid:peripheral.advertising.serviceUUIDs})
+        //if(peripheral.id === 'mac da esp' && peripheral.advertising.serviceUUIDs === 'uuid da esp')
+        try
+        {
+            await firebase.database().ref(`usersTeste/${this.state.user.uid}/${peripheral.id}`).set({timestamp: Date.now(),uuid:peripheral.advertising.serviceUUIDs})
+        }
+        catch(err)
+        {
+            console.log(err)
+        }
     }
     render()
     {
         return (
             <Container>
-                <Icon name="bluetooth-b" size={200} color="white" />
+                <Animatable.View animation="tada" iterationCount="infinite" easing="linear">
+                    <Icon name="bluetooth-b" size={200} color="white" />
+                </Animatable.View>
                 <AlertText>Mantenha seu bluetooth ligado para confimar sua presença</AlertText>
             </Container>
         )
