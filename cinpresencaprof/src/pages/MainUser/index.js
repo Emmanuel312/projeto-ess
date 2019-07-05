030c3a09a31d720f5026f1e8631ed5d9879ccfc0
Emmanuel312 <emmanuelfelipe@viitra.com>
2019-07-05 02:16:14 -0300

    design quase feito

--- cinpresencaprof/src/pages/MainUser/index.js
+++ cinpresencaprof/src/pages/MainUser/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { firebaseDatabase,firebaseImpl } from '../../firebase/config'
-import { Time } from './styles';
+import { MainText,TimeInicio,TimeFim,Container,Header,List,InputText,Button,H2,Menu,MenuContent,TextMenu,MainContent,Main } from './styles'
+import Modal from 'react-responsive-modal'
 
 export default class MainUser extends Component
 {
@@ -10,35 +11,95 @@ export default class MainUser extends Component
         start: '',
         end: '',
         name: '',
+        usersUid: [],
+        open: false,
     }
 
     async componentDidMount()
     {
         const user = await firebaseImpl.auth().currentUser
         this.setState({ user })
+        console.log(user)
+        this.listener = firebaseDatabase.ref('/users').once('value', this.getUserUid)
+
+    }
+
+    getUserUid = snapshot =>
+    {
+        this.setState({ usersUid: Object.keys(snapshot.val()) })
     }
 
     schedule = async () =>
     {
         const start = new Date(this.state.start).getTime()
         const end = new Date(this.state.end).getTime()
-        console.log(start)
-        await firebaseDatabase.ref(`agenda/${this.state.user.uid}`).set({alunos_presentes:[{id1:12323443},{id2:1293298}],fim:end,inicio:start,nome_aula:this.state.name})
+        await firebaseDatabase.ref('agenda').push({alunos_presentes:this.state.usersUid.map(userUid =>
+        {
+            return {
+                quantidade_parcial_presencas: 0,
+                referencia: `users/${userUid}`
+            }
+        }),fim:end,inicio:start,nome_aula:this.state.name})
     }
 
-    handleTime = e =>
+    handleInput = e =>
     {
         this.setState({[e.target.name]:e.target.value})
+    }
+
+    onOpenModal = () =>
+    {
+        this.setState({ open: true })
+    }
+
+    onCloseModal = () =>
+    {
+        this.setState({ open: false })
     }
 
     render()
     {
         return (
+            
             <section>
-                <input name="name" type="text" value={this.state.name} placeholder="Nome da aula" onChange={this.handleTime} />
-                <Time name="start" inputType="datetime-local" step="1" value={this.state.start} onChange={this.handleTime} />
-                <Time name="end" inputType="datetime-local" step="1" value={this.state.end} onChange={this.handleTime} />
-                <button onClick={this.schedule}>Agendar aula</button>
+                <Menu>
+                    <MenuContent>
+                        <TextMenu>CinPresença</TextMenu>
+                    </MenuContent>
+                </Menu>
+                <Main>
+                    <MainContent>
+                        <MainText>Seja bem vindo Kiev!</MainText>
+
+
+                        <Button style={{backgroundColor:'#151515',width:'40%',marginTop:'20px'}} onClick={this.onOpenModal}>Open modal</Button>
+                        <List>
+                            
+                            
+                            
+                        </List>
+                        
+                        
+                        
+                        
+                        
+                        <Modal showCloseIcon={false} styles={{bordeRadius:'20px'}} open={this.state.open} onClose={this.onCloseModal} center style={{borderRadius:'10'}}>
+                        <Container>
+                            <Header>
+                                <H2>Cadastro de aula</H2>
+                            </Header>
+                            <InputText name="name" type="text" value={this.state.name} placeholder="Nome da aula" onChange={this.handleInput} />
+                            <TimeInicio name="start" inputType="datetime-local" step="1" value={this.state.start} onChange={this.handleInput} />
+                            <TimeFim name="end" teste="ok" inputType="datetime-local" step="1" value={this.state.end} onChange={this.handleInput} />
+                            <Button onClick={this.schedule}>Agendar aula</Button>
+                        </Container>
+                    </Modal>
+
+                    </MainContent>
+                
+                </Main>
+                
+                
             </section>
             
         )
--- cinpresencaprof/src/pages/MainUser/styles.js
+++ cinpresencaprof/src/pages/MainUser/styles.js
@@ -1,14 +1,143 @@
 import styled from 'styled-components'
 
-export const Time = styled.input.attrs(props =>
+export const Menu = styled.header`
+    background: #50B4F2;
+    height: 8vh;
+`;
+
+export const MenuContent = styled.div`
+    width: 100%;
+    max-width: 980px;
+    margin: 0 auto;
+    height: 8vh;
+    padding: 0 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const TextMenu = styled.span`
+    @import url('https://fonts.googleapis.com/css?family=Sofadi+One&display=swap');
+    font-size: 20px;
+    color: #fff;
+    margin-right: 15px;
+    font-family: 'Sofadi One', cursive;
+`;
+export const TimeInicio = styled.input.attrs(props =>
 ({
     type: props.inputType,
 }))
 `
-    font-family: 'PT Sans', sans-serif;
-    font-size: 0.8rem;
-    display: block;
-    padding: 0.2rem 0.5rem;
-    border-radius: 5px;
-    width: 200px;
+    margin-bottom:20px;
+    margin-top:10px;
+    width:100%;
+    padding:15px;
+    border-radius:5px;
+    border:1px solid #7ac9b7;
+    font-size:18px;
+    :before {
+    content:'Inicio:';
+    margin-right:.6em;
+    color:#9d9d9d;
+    }
 ` 
+
+export const TimeFim = styled.input.attrs(props =>
+    ({
+        type: props.inputType,
+    }))
+    `
+        margin-bottom:20px;
+        margin-top:10px;
+        width:100%;
+        padding:15px;
+        border-radius:5px;
+        border:1px solid #7ac9b7;
+        font-size:18px;
+        :before {
+        content:'Fim:';
+        margin-right:.6em;
+        color:#9d9d9d;
+        }
+    `     
+
+export const Container = styled.div`
+    max-width:600px;
+    margin:auto;
+    padding:10px;
+    font-size:18px;
+    font-weight:bold;
+
+`
+export const Header = styled.div`
+    background-color:#50B4F2;
+    text-align:center;
+    max-width:600px;
+    color:aliceblue;
+    padding:10px;
+    margin:auto;
+    border:1px solid gray;
+    border-bottom:none;
+
+`
+export const H2 = styled.div`
+    margin:0;
+    padding:15px;
+`    
+export const InputText = styled.input`
+    margin-bottom:20px;
+    margin-top:10px;
+    width:100%;
+    padding:15px;
+    border-radius:5px;
+    border:1px solid #7ac9b7;
+    font-size:18px;
+`
+
+export const Button = styled.button`
+    margin-bottom:20px;
+    width:100%;
+    padding:15px;
+    border-radius:5px;
+    border:1px solid #7ac9b7;
+    background:#50B4F2;
+    color:aliceblue;
+    cursor:pointer;
+    font-weight:bold;
+    font-size:18px;
+`
+export const Main = styled.div`
+    margin-top: 10px;
+    height: 92vh;
+`
+export const MainContent = styled.div`
+    width: 100%;
+    max-width: 980px;
+    margin: 0 auto;
+    height: 92vh;
+    background: #fff;
+    padding: 30px 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const List = styled.div`
+    width: 98%;
+    max-width: 980px;
+    background: gray;
+    padding: 30px 30px;
+    height: 90%;
+    overflow:auto;
+    display: flex;
+    flex-direction: column;
+    
+`
+
+
+export const MainText = styled.span`
+    @import url('https://fonts.googleapis.com/css?family=Sofadi+One&display=swap');
+    font-size: 20px;
+    font-family: 'Sofadi One', cursive;
+`
+
